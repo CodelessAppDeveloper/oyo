769238b0d6b3a41468924ee7c1fbbda70b78e004
Suhas Anand Balagar <suhasbalagar@gmail.com>
2022-05-15 12:38:45 -0700

    try

--- frontend/customer/src/controllers/checkout.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import * as Const from "../constants";
-
-const semiEndpoint = Const.semiEndpoint;
-// Stub for making the REST call
-export function confirmBooking(dataJson, token) {
-  axios.defaults.headers.common["authorization"] = token;
-  return axios.post(semiEndpoint + "/checkout", dataJson);
-}
--- frontend/customer/src/utils/ApiEndpoint.js
+++
@@ -1,1 +0,0 @@
-export const semiEndpoint = "http://localhost:3000";
