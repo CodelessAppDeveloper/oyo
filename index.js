44e8ebaf74c4505fe8926fa2b95329b5533d4c5c
Suhas Anand Balagar <suhasbalagar@gmail.com>
2022-05-15 15:14:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ const apiRouter = require("./routes/routes");
 const roomRouter = require('./routes/room_routes');
 const bookingsRouter = require('./routes/booking_routes');
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hotel Booking Backend API endpoint</h1> <h4>Message: Success</h4> <p>Team Scrumdog Millionaires</p>');
+})
+
 app.use("/", apiRouter);
 app.use("/rooms", roomRouter);
 app.use("/bookings", bookingsRouter);
